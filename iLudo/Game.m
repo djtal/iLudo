15fa936c5c4d0a09ca74969c50be6e46a6791e2c
djtal <guillaume.garcera@gmail.com>
2011-08-15 18:41:10 +0200

    add game name validation

--- iLudo/Game.m
+++ iLudo/Game.m
@@ -16,4 +16,18 @@
 @dynamic min_player;
 @dynamic max_player;
 
+- (BOOL)validateName:(id*)ioValue error:(NSError**)outError{
+    NSString *proposedName = *ioValue;
+    if (!proposedName || [proposedName isEqualToString:@" "]) {
+        NSString *localizedDesc = NSLocalizedString(@"Vous devez saisir un nom", @"Vous devez saisir un nom");
+        NSDictionary *errorUserInfo = [NSDictionary dictionaryWithObject:localizedDesc forKey:NSLocalizedDescriptionKey];
+        if (outError) {
+            *outError = [NSError errorWithDomain:NSCocoaErrorDomain code:NSValidationStringTooShortError userInfo:errorUserInfo];
+        }
+        return NO;
+    }
+    return YES;
+    
+}
+
 @end
